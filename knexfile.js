99afd392dcc360499ffe6d85543c469857004da3
denbon05 <denisamigoua@gmail.com>
2021-05-09 17:21:51 +0200

    Change connection config for heroku deploy

--- knexfile.js
+++ knexfile.js
@@ -26,7 +26,16 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: {
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: 5432,
+      host: 'ec2-54-247-158-179.eu-west-1.compute.amazonaws.com',
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
     useNullAsDefault: true,
     migrations,
   },
