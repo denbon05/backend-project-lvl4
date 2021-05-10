9a89b184a1626e7bfcd7b100afc2948bf27386b3
denbon05 <denisamigoua@gmail.com>
2021-05-10 21:32:42 +0200

    Change db client to sqlite3 for CI

--- knexfile.js
+++ knexfile.js
@@ -25,16 +25,9 @@ module.exports = {
     migrations,
   },
   production: {
-    client: 'pg',
+    client: 'sqlite3',
     connection: {
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      port: 5432,
-      host: 'ec2-54-247-158-179.eu-west-1.compute.amazonaws.com',
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      filename: './database.sqlite',
     },
     useNullAsDefault: true,
     migrations,
