8aee551c33f034be44eeb3195c627d844b5ec2cc
denbon05 <denisamigoua@gmail.com>
2021-05-13 13:13:07 +0200

    Temporary commit

--- knexfile.js
+++ knexfile.js
@@ -11,9 +11,16 @@ const migrations = {
 
 module.exports = {
   development: {
-    client: 'sqlite3',
+    client: 'postgresql',
     connection: {
-      filename: './database.sqlite',
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: 5432,
+      host: 'ec2-54-247-158-179.eu-west-1.compute.amazonaws.com',
+      ssl: {
+        rejectUnauthorized: false,
+      },
     },
     useNullAsDefault: true,
     migrations,
