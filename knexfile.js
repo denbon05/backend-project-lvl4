1999f206cd718a013edfa654dfb77da384a47a7a
denbon05 <denisamigoua@gmail.com>
2021-05-13 14:39:34 +0200

    Add rollbar

--- knexfile.js
+++ knexfile.js
@@ -9,6 +9,31 @@ const migrations = {
   directory: path.join(__dirname, 'server', 'migrations'),
 };
 
+const herokuConfig = {
+  client: 'postgresql',
+  connection: {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: 5432,
+    host: 'ec2-54-247-158-179.eu-west-1.compute.amazonaws.com',
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  },
+  useNullAsDefault: true,
+  migrations,
+};
+
+const githubConfig = {
+  client: 'sqlite3',
+  connection: {
+    filename: './database.sqlite',
+  },
+  useNullAsDefault: true,
+  migrations,
+};
+
 module.exports = {
   development: {
     client: 'sqlite3',
@@ -24,19 +49,5 @@ module.exports = {
     useNullAsDefault: true,
     migrations,
   },
-  production: {
-    client: 'postgresql',
-    connection: {
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      port: 5432,
-      host: 'ec2-54-247-158-179.eu-west-1.compute.amazonaws.com',
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    },
-    useNullAsDefault: true,
-    migrations,
-  },
+  production: process.env.IS_HEROKU_ENV ? herokuConfig : githubConfig,
 };
