136aa468dd1fb02467dcdccd4560e30a6709d8ef
denbon05 <denisamigoua@gmail.com>
2021-07-10 17:26:49 +0200

    Update view for hexlet auto tests

--- src/index.js
+++ src/index.js
@@ -2,3 +2,13 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './tasks_icon.png';
+
+const collapseBtn = document.querySelector('.navbar-toggler');
+collapseBtn.addEventListener('click', () => {
+  const navbarColapseEl = document.getElementById('navbarToggleExternalContent');
+  if (navbarColapseEl.classList.contains('show')) {
+    navbarColapseEl.classList.remove('show');
+  } else {
+    navbarColapseEl.classList.add('show');
+  }
+});
